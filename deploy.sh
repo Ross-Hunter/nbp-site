81b31397ac7977d362c0811409996dcbd937abfe
Ross-Hunter <ross-hunter@ross-hunter.com>
2017-12-09 10:12:58 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 
 #!/bin/sh
-
-DIR=$(dirname "$0")
-
-cd $DIR/..
-
 if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
@@ -27,4 +22,4 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push && cd ..
